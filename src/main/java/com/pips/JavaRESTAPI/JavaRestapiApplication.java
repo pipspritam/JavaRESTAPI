2967329d928e6dae67ff2372b4e9dad5d6884faa
Pritam Mondal <pritammondalpips@gmail.com>
2023-09-10 12:54:08 +0530

    first

---
+++ src/main/java/com/pips/JavaRESTAPI/JavaRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.pips.JavaRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pips/JavaRESTAPI/JavaRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pips.JavaRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
