5553c9dd3ed8914c64f519f1fb9b16bf10814c12
Pritam Mondal <pritammondalpips@gmail.com>
2023-09-17 02:17:41 +0530

    allow cross platform

--- src/main/java/com/pips/JavaRESTAPI/controller/Controller.java
+++ src/main/java/com/pips/JavaRESTAPI/controller/Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.pips.JavaRESTAPI.service.Service;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200")
 
 public class Controller {
     @Autowired
